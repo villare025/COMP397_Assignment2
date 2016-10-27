322b8acf37dd9a2c6bae41fba6ba732c155b98af
villare025 <empvillare@yahoo.com>
2016-10-26 21:09:34 -0400

    Make Instruction Scene Render and Work

--- Scripts/scenes/instructions.js
+++ Scripts/scenes/instructions.js
@@ -6,7 +6,7 @@
     Website Name:          EV - COMP397 - Assignment 2
     Program Description:   JS file that contains the components that
                            are required to render the game's Instructions scene.
-    Revision History:      Initial Commit
+    Revision History:      Make Instruction Scene Work
 */
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
@@ -26,22 +26,16 @@ var scenes;
             // Add objects to the scene
             console.log("Game scene started");
             // Create BG for scene and add to Game Scene container
-            this._bg = new createjs.Bitmap(assets.getResult("BG_Title"));
+            this._bg = new createjs.Bitmap(assets.getResult("BG_Instr"));
             this.addChild(this._bg);
             // Add START Button to scene. Register for click callback function
-            this._instructionsBtnStart = new objects.Button("Start", config.Screen.CENTER_X + 135, config.Screen.CENTER_Y + 110);
+            this._instructionsBtnStart = new objects.Button("BTN_Play", config.Screen.CENTER_X + 150, config.Screen.CENTER_Y + 160);
             this.addChild(this._instructionsBtnStart);
             this._instructionsBtnStart.on("click", this._startButtonClick, this);
             // Create RETURN Button for scene and add to Game Scene container. Register for onclick event
-            this._instructionsBtnBack = new objects.Button("Return", config.Screen.CENTER_X + 280, config.Screen.CENTER_Y + 200);
+            this._instructionsBtnBack = new objects.Button("BTN_Back", config.Screen.CENTER_X - 190, config.Screen.CENTER_Y + 160);
             this.addChild(this._instructionsBtnBack);
             this._instructionsBtnBack.on("click", this._onBackButtonClick, this);
-            // Create Label for scene and add to Game Scene container
-            this._instructionsTitle = new objects.Label("instructions", "60px Kaushan Script", "#000000", config.Screen.CENTER_X + 55, config.Screen.CENTER_Y - 150);
-            this.addChild(this._instructionsTitle);
-            // Create Label for scene and add to Game Scene container
-            this._instructionsParagraph = new objects.Label("Instructions: \nSimply click on a golden choice button \n     to progress through the story. \nThere are a total of 8 endings \n - 7 Bad Endings and 1 True Good End. \nA Return Button is available \n     on the Top Left to go back a scene. \nA Switch Button is available \n     on the Top Right to go the other scene.", "bold 14px Verdana", "#000000", config.Screen.CENTER_X + 140, config.Screen.CENTER_Y);
-            this.addChild(this._instructionsParagraph);
             // Add gamescene to main stage container. 
             stage.addChild(this);
         };
